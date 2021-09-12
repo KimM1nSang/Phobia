d00a8cdd5e9e3c2b29129760cd087ebb95d4f6d5
DESKTOP-77DPHAE\ROG <3soojung@gmail.com>
2021-09-13 00:56:33 +0900

    플레이어 프리팹으로 바꿈 카메라 움직임 변수로 뺌 빈 스크립트 추가

--- Assets/01.Scripts/Player/PlayerCamera.cs
+++ Assets/01.Scripts/Player/PlayerCamera.cs
@@ -4,22 +4,28 @@ using UnityEngine;
 
 public class PlayerCamera : MonoBehaviour
 {
-    private float time;
     public PlayerInput input;
+    public Transform viewPoint;
+
+    private float time;
     private float yMove = 0;
+    public float interval = 0.1f;
+
     // Start is called before the first frame update
     void Awake()
     {
+        input = GetComponent<PlayerInput>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        time += Time.deltaTime*10;
+        
         if (input.frontMove != 0 || input.rightMove !=0)
         {
-            yMove = Mathf.Sin(time)*0.1f;
-            transform.position = new Vector3(transform.position.x,transform.parent.position.y+yMove, transform.position.z);
+            time += Time.deltaTime * 10;
+            yMove = Mathf.Sin(time)*interval;
+            transform.position = new Vector3(viewPoint.transform.position.x, viewPoint.transform.position.y+yMove, viewPoint.transform.position.z);
         }
     }
 }

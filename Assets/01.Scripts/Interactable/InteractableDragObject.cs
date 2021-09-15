2c59a4603698ac983583780fbe129c27f7cfb9ad
KimM1nSang <minsang1008@naver.com>
2021-09-15 11:50:22 +0900

    병함 과정 오류 수정

--- Assets/01.Scripts/Interactable/InteractableDragObject.cs
+++ Assets/01.Scripts/Interactable/InteractableDragObject.cs
@@ -23,12 +23,9 @@ public class InteractableDragObject : InteractableObject
 	protected override void Update()
 	{
 		base.Update();
-<<<<<<< HEAD
 		Debug.Log("¹«¾ßÈ£");
 		if(canRotate && !PlayerInput.instance.interactUp)
 			transform.parent.rotation = Quaternion.Euler(0, Mathf.Clamp(transform.parent.rotation.eulerAngles.y + PlayerInput.instance.mouseY, 0, 90), 0);
-		if (PlayerInput.instance.interactUp && canRotate)
-=======
 		if (Physics.OverlapBox(transform.position, meshSize.size, Quaternion.identity, whatIsPlayer) != null)
 		{
 			Debug.Log(Physics.OverlapBox(transform.position * transform.position.y, meshSize.size, Quaternion.identity, whatIsPlayer).Length);
@@ -50,7 +47,6 @@ public class InteractableDragObject : InteractableObject
 		}
 			
 		if (PlayerInput.instance.interactUp&& canRotate)
->>>>>>> 06c4efa07aefe6696ecc1e65f97fb4dff5c2ed6f
 		{
 			canRotate = false;
 			GameManager.instance.canLook = true;

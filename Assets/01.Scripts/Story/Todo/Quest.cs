afd8c55546d4b434557e29b97883d649dddb9601
KimM1nSang <minsang1008@naver.com>
2021-12-06 17:57:19 +0900

    파일 정리, 프리팹 정리

---
+++ Assets/01.Scripts/Story/Todo/Quest.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "new Quest Object", menuName = "Asset/New Quest Object")]
+public class Quest : ScriptableObject
+{
+
+}
